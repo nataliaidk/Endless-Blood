41c1ba66395abb1eb0a2ac5789132480af6b5319
marg4ryn <poweski.jan@gmail.com>
2026-03-27 13:26:20 +0100

    Healthbar

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -2,13 +2,18 @@ extends CharacterBody2D
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var player = get_tree().get_first_node_in_group("player")
-@onready var progress_bar: ProgressBar = $ProgressBar
+@onready var health_bar: TextureProgressBar = $TextureProgressBar
 
 var speed := 75
-var max_health := 10
-var damage := 5
+var max_health := 100
 var health := max_health
+var damage := 25
+var knockback := 5
 var is_dead := false
+
+func _ready():
+	health_bar.value = health
+	health_bar.max_value = max_health
 
 func _physics_process(_delta):
 	if is_dead or player == null:
@@ -30,19 +35,12 @@ func update_animation(direction: Vector2):
 			sprite.play("walk_down")
 		else:
 			sprite.play("walk_up")
-			
-func _on_hitbox_body_entered(body):
-	if is_dead:
-		return
-	if body.is_in_group("player"):
-		print("damage dealt")
-		body.take_damage(damage)
 
-func take_damage(amount: int):
+func take_damage(attack: Attack):
 	if is_dead:
 		return
-	health -= amount
-	progress_bar.value = health
+	health -= attack.damage
+	health_bar.value = health
 	if health <= 0:
 		die()
 
@@ -51,3 +49,14 @@ func die():
 	velocity = Vector2.ZERO
 	sprite.play("die")
 	queue_free()
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	print("Body:", body)
+	if is_dead:
+		return
+	if body.is_in_group("player"):
+		var attack = Attack.new()
+		attack.damage = damage
+		attack.knockback = knockback
+		attack.position = global_position
+		body.take_damage(attack)
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,15 +2,17 @@ extends CharacterBody2D
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var camera := $Camera2D
-@onready var progress_bar: ProgressBar = $ProgressBar
+@onready var health_bar: TextureProgressBar = $TextureProgressBar
 
 var speed := 200.0
 var max_health := 100
-var health := 100
+var health := max_health
 var is_dead := false
 
 func _ready():
 	camera.make_current()
+	health_bar.value = health
+	health_bar.max_value = max_health
 
 func _physics_process(_delta):
 	if is_dead:
@@ -35,14 +37,14 @@ func update_animation(direction: Vector2):
 			else:
 				sprite.play("walk_up")
 
-func take_damage(amount: int):
+func take_damage(attack: Attack):
 	if is_dead:
 		return
-	progress_bar.value = health
-	health -= amount
+	health -= attack.damage
+	health_bar.value = health
 	if health <= 0:
 		die()
-		
+
 func die():
 	is_dead = true
 	velocity = Vector2.ZERO
